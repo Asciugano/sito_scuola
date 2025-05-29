43ca5e2d7f2701b7e8e220d1f1db90d2a77e7a98
Asciugano <samuele.diaferio@fastwebnet.it>
2025-05-29 19:45:59 +0200

    aggiunte le prime dipendenze

---
+++ src/main/java/com/asciugano/sito_scuola/SitoScuolaApplication.java
@@ -0,0 +1,13 @@
+package com.asciugano.sito_scuola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitoScuolaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SitoScuolaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/asciugano/sito_scuola/SitoScuolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asciugano.sito_scuola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitoScuolaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
